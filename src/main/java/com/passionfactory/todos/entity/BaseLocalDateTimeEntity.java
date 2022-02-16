e738f9309d7e5ee8046affe95bbc008892fe8a47
seonghyeoklee <dltjdgur327@gmail.com>
2022-02-17 02:14:55 +0900

    @EnableJpaAuditing 설정

---
+++ src/main/java/com/passionfactory/todos/entity/BaseLocalDateTimeEntity.java
@@ -0,0 +1,25 @@
+package com.passionfactory.todos.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseLocalDateTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
